f752fcc37bcd0a5e66150efbc2fb8cc7666fe829
Sage Arbor <sagearbor@gmail.com>
2016-04-12 11:11:59 -0400

    Change index to .php.  Working but need to make and use sidebar.php still.

---
+++ sidebar.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+
+<!--  http://www.medcurriculum.org/  -->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+<html>
+
+<head>  
+<title> Medical School objectives DB </title><br>
+<h1 class="text-center"> Medical School objective DB  </h1><br>
+<h2 class="text-center"> A searchable database from multiple national societies </h2><br>
+<link type="text/css" rel="stylesheet"> </link>
+</head>
+
+<body>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+Test page med objv, cant be right STILL , at ... /var/www/html/bitlyMedObjsV0p03/public_html <br>
+
+<br>
+<br>
+
+
+<br>
+
+<br>
+
+<form action="sqlQuery.php" method="post" class="form-horizontal" role="form">
+<div class="row">
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" name="bt1" aria-label="...">
+      </span>
+      <input type="text" name="searchTerm1" class="form-control" aria-labelledby="...WHAT">
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <!-- <input type="radio" name="bt2" aria-label="...">    -->
+        <input type="checkbox" name="bt2" aria-label="...">
+      </span>
+      <input type="text" name="searchTerm2" class="form-control" aria-label="...">
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+</div><!-- /.row -->
+<input type="submit">
+</form>
+
+<br>
+
+
+<br>
+<br>
+this was controlled by changing default path in mysite.conf
+  33
+<br>
+
+<div class="row">
+<div class="col-12 col-sm-3 col-lg-3 left">right side bar
+
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Societies
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+<div class="row">
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Anatomy</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 --><br>
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Anesthesia</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Behavioral Sciences (Psych)</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Biochemistry</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Cell Biology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Clinical Medicine</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Embryology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Family Medicine</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Genetics  </span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Gerontology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Hemtology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Histology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Internal Medicine</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Microbiology/Immunology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Neuroscience</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Obstetrics & Gynecology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Pathology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Pediatrics</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Pharmacology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Physiology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+  <div class="col-lg-6">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" checked aria-label="...">
+      </span><span> Radiology</span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+</div><!-- /.row -->
+      </div>
+    </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Disciplines
+        </a>
+      </h4>
+    </div>
+
+ 
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Year (more recent than)   
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+        Make drop down with years here 
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>6th line
+
+</div>
+</div>
+
+<br>
+
+
+<div class="col-12 col-sm-9 col-lg-9 mn">
+main content<br>
+testing apache default location when started as roFromWeb<br>
+<b>2nd line,</b><br>
+  
+</form>
+  </div></div></div>
+
+<br>5th line
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>6th line
+
+</div>
+</div>
+
+</body>
+
+
+     <div id="footer">
+          <div class="wrap">
+               <p>This would be your footer content!</p>
+          </div>
+     </div>
+
+<!--   <footer class="text-right">   -->
+<footer>
+    <a href="./contact.html">Contact</a>
+    <a href="./about.html">About</a>
+    <a href="./help">Help</a>
+</footer>
+
+</html>
